aae50a70751f13751bb402fc4bc18ffa87177f6c
Stanislav Kohut <stako211@gmail.com>
2024-07-27 10:46:55 +0300

    27072024_1046 cgange connected to db

--- api/db.cjs
+++ api/db.cjs
@@ -1,26 +1,26 @@
 const mysql = require("mysql2/promise");
 
-// const pool = mysql.createPool({
-//   host: "127.0.0.1",
-//   user: "root",
-//   password: "Fl1p2ua.fm",
-//   port: "3306",
-//   database: "auth",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// })
-
 const pool = mysql.createPool({
-  host: "192.168.1.200",
-  user: "sqluser",
-  password: "Teflon08",
+  host: "127.0.0.1",
+  user: "root",
+  password: "Fl1p2ua.fm",
   port: "3306",
   database: "auth",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 })
+
+// const pool = mysql.createPool({
+//   host: "192.168.1.200",
+//   user: "sqluser",
+//   password: "Teflon08",
+//   port: "3306",
+//   database: "auth",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// })
 
 // export default pool;
 module.exports = pool;
