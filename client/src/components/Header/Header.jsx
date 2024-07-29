ea28e767cd55081cd9400587eb2f07585bfd7c7a
Stanislav Kohut <stako211@gmail.com>
2024-07-29 11:09:06 +0300

    add header

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { memo } from "react";
+import cn from "classnames";
+import style from "./Header.scss";
+
+export default memo(
+  ({ register, name, error = false, helperText = "", ...rest }) => {
+    return (
+      <header className="header">
+
+      </header>
+    );
+  }
+);
