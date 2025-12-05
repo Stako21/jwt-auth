4d9c79366522d2b86634693e1decd118d77bcbd0
Stanislav Kohut <stako211@gmail.com>
2025-12-05 19:00:51 +0200

    befor deploy

--- api/db.cjs
+++ api/db.cjs
@@ -1,19 +1,8 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Fl1p2ua.fm",
-  port: "3306",
-  database: "auth",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-})
-
 // const pool = mysql.createPool({
 //   host: "localhost",
-//   user: "stako",
+//   user: "root",
 //   password: "Fl1p2ua.fm",
 //   port: "3306",
 //   database: "auth",
@@ -21,6 +10,17 @@ const pool = mysql.createPool({
 //   connectionLimit: 10,
 //   queueLimit: 0
 // })
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "stako",
+  password: "Fl1p2ua.fm",
+  port: "3306",
+  database: "auth",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
 
 // export default pool;
 module.exports = pool;
--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,10 @@
-export default {
-  API_URL: "http://localhost:5000",
-  LOGOUT_STORAGE_KEY: "logout",
-};
-
 // export default {
-//   API_URL: "https://balance.roshen.zp.ua",
+//   API_URL: "http://localhost:5000",
 //   LOGOUT_STORAGE_KEY: "logout",
 // };
+
+export default {
+  API_URL: "https://balance.roshen.zp.ua",
+  LOGOUT_STORAGE_KEY: "logout",
+};
 
