0db528183c02a0c10633e8dc622d67eb43a566b5
Stanislav Kohut <stako211@gmail.com>
2026-02-26 08:45:02 +0200

    change in schebuler loadReports

--- api/services/scheduler.js
+++ api/services/scheduler.js
@@ -1,42 +1,44 @@
-import cron from 'node-cron';
-import { loadSalesAgents } from './loadAgents.js';
-import { loadSalesReports } from './loadReports.js';
-import { loadProducts } from './loadProducts.js';
-import { loadTradePoints } from './loadTradePoints.js';
-import { retryFailedNotifications } from './notify.retry.service.js';
+import cron from "node-cron";
+import { loadSalesAgents } from "./loadAgents.js";
+import { loadSalesReports } from "./loadReports.js";
+import { loadProducts } from "./loadProducts.js";
+import { loadTradePoints } from "./loadTradePoints.js";
+import { retryFailedNotifications } from "./notify.retry.service.js";
 
 // 1️⃣ Обновление SalesAgent раз в сутки в 8:00
 // cron.schedule('0 8 * * *', () => {
 //   console.log('Запуск обновления SalesAgent.json');
 //   loadSalesAgents();
 // });
+// Последнее обновление loadReports
 
-setInterval(() => {
-  console.log('Запуск обновления SalesAgent.json');
-  loadSalesAgents();
-}, 2 * 60 * 60 * 1000)
-
+setInterval(
+  () => {
+    console.log("Запуск обновления SalesAgent.json");
+    loadSalesAgents();
+  },
+  2 * 60 * 60 * 1000,
+);
 
 // 2️⃣ Проверка SalesReport.json каждые 30 секунд
 setInterval(() => {
-  console.log('Проверка SalesReport.json');
+  console.log("Проверка SalesReport.json");
   loadSalesReports();
 }, 30 * 1000);
 
-
 // каждые 2 часа 2 * 60 * 60 * 1000
 setInterval(() => {
-  console.log('Проверка Product.json');
+  console.log("Проверка Product.json");
   loadProducts();
 }, 60 * 1000);
 
 // каждые 2 часа (можно со сдвигом)
 setInterval(() => {
-  console.log('Проверка TradePoint.json');
+  console.log("Проверка TradePoint.json");
   loadTradePoints();
 }, 60 * 1000);
 
-cron.schedule('*/5 * * * *', async () => {
-  console.log('Запуск повторных попыток уведомлений');
+cron.schedule("*/5 * * * *", async () => {
+  console.log("Запуск повторных попыток уведомлений");
   retryFailedNotifications();
 });
